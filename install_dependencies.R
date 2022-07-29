7fb87729e14f6d106888fae4ad3c4815977d1302
eneskemalergin <eneskemalergin@gmail.com>
2022-07-28 20:35:50 -0700

    Fix and update package installer

--- install_dependencies.R
+++ install_dependencies.R
@@ -3,17 +3,15 @@
 # Modified by Enes Kemal Ergin
 # Uses *_packages.txt files in dep/ folder to get list of packages to check and install
 
-install_opts <- "--byte-compile"
-reinstall <- FALSE
+
+install_opts <- "--byte-compile" # Allow bytecompile
+reinstall <- FALSE # Don't reinstall
 
 # Installation functions
 installed <- data.frame(installed.packages())$Package
-# Get the bioconductor from source
-source("http://bioconductor.org/biocLite.R")
-
 # function that checks if packages are installed
 is.installed <- function(package.name) package.name %in% installed
-
+# Message printer
 pastemsg <- function(...) message(paste(...))
 
 # base installer function modified with installation checker and message printing
@@ -28,36 +26,17 @@ installer <- function(install.fn, ...) {
   }
 }
 
+## Install CRAN packages
 # Special installer for CRAN
 install_cran <- installer(install.packages, repos="http://cran.fhcrc.org",
                           clean=TRUE, dependencies=TRUE,
                           INSTALL_opts=install_opts)
 
-# Special installer for bioconductor
-install_bioconductor <- installer(biocLite, ask=FALSE, INSTALL_opts=install_opts)
-
-## Bioconductor Packages
-# Read the file to get the list of bioconductor packages
-bioconductor_ <- paste(readLines('./dep/bioconductor_packages.txt'), sep = "\n")
-# Check if the file has packages
-if(length(bioconductor_)){
-  print("Calling for the bioconductor source!")
-  # Call BiocInstaller Library to install bioconductor
-  library(BiocInstaller)
-  # Get the non-installed packages into list
-  bioconductor_packages <- bioconductor_[!(bioconductor_ %in% installed.packages()[, "Package"])]
-  # install bioconductor_packages
-  for(p in bioconductor_packages) {
-    tryCatch(install_bioconductor(p), error=warning)
-  }
-}
-
-## CRAN Packages
 # Read the files to get the list of cran packages
 cran_ <- paste(readLines('./dep/cran_packages.txt'), sep = "\n")
 # Check if the file has packages
 if(length(cran_)){
-  print("Installing Packages from Cran")
+  print("Installing Packages from CRAN")
   # Get the non-installed packages into list
   cran_packages <- cran_[!(cran_ %in% installed.packages()[, "Package"])]
   # install CRAN packages
@@ -67,6 +46,10 @@ if(length(cran_)){
 }
 
 ## GitHub Package
+# Get the devtools from source if not installed
+if(!require("devtools", quietly=TRUE))
+  install.packages("devtools")
+
 # Read the files to get the list of GitHub packages
 github_ <- paste(readLines('./dep/github_packages.txt'), sep="\n")
 # Check if the file has packages
@@ -77,8 +60,26 @@ if(length(github_)){
   # Install GitHub packages
   for(p in github_packages) {
     pastemsg("Installing", p, "from GitHub")
-    p <- list(p)
-    p$args <- install_opts
-    tryCatch(do.call('install_github', p), error=warning)
+    tryCatch(devtools::install_github(p, force=reinstall, upgrade="never"), error=warning)
+  }
+}
+
+## Install Bioconductor Packages
+
+# Get the bioconductor from source
+if (!require("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+
+# Read the file to get the list of bioconductor packages
+bioconductor_ <- paste(readLines('./dep/bioconductor_packages.txt'), sep = "\n")
+# Check if the file has packages
+if(length(bioconductor_)){
+  print("Calling for the bioconductor source!")
+  # Get the non-installed packages into list
+  bioconductor_packages <- bioconductor_[!(bioconductor_ %in% installed.packages()[, "Package"])]
+  # install bioconductor_packages
+  for(p in bioconductor_packages) {
+    pastemsg("Installing", p, "using Bioconductor")
+    tryCatch(BiocManager::install(p, ask=FALSE, force=reinstall), error=warning)
   }
 }
